80cb1888b184de00f224206667c9f9f4b5873116
Abuzar <abuzar@maxerience.com>
2022-11-30 11:31:31 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predicting the need of wafer replacement using MLOPS',
+    author='Md Abuzar',
+    license='MIT',
+)
